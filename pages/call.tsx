d0ac41eb6a3f564e79b560e5cdb4034b2875cdc1
Pavel Belyaev <zommerberg@abv.bg>
2020-08-12 16:20:55 +0300

    configur esignaling server

--- pages/call.tsx
+++ pages/call.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import Camera from '../components/Camera'
 import Stream from '../components/Stream'
 
-const SERVER_URL = 'http://localhost:8000/'
+const SERVER_URL = 'http://penguin-signaling-server.herokuapp.com/'
 
 const { useEffect, useState } = React
 const Call = () => {
