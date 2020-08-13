57c18641a9b161697e4dd8914b57cdf5b3719181
Pavel <zommerberg@abv.bg>
2020-08-13 20:28:56 +0300

    ios setup

--- pages/call.tsx
+++ pages/call.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link'
 import Camera from '../components/Camera'
 import Stream from '../components/Stream'
 
-const SERVER_URL =
-  'http://localhost:8000/' || 'https://penguin-signaling-server.herokuapp.com/'
+const SERVER_URL = 'https://penguin-signaling-server.herokuapp.com/'
 
 const { useEffect, useState } = React
 const Call = () => {
